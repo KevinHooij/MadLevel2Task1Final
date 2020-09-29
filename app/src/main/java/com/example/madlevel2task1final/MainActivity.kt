f394795149c19a7be9c9187df5761a6485b4f41c
KevinHooij <kevin.hooijschuur@hva.nl>
2020-09-29 14:59:14 +0200

    Finishes Level 2 Task 2

--- app/src/main/java/com/example/madlevel2task1final/MainActivity.kt
+++ app/src/main/java/com/example/madlevel2task1final/MainActivity.kt
@@ -2,15 +2,33 @@ package com.example.madlevel2task1final
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.madlevel2task1final.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val places = arrayListOf<Place>()
+    private val placeAdapter = PlaceAdapter(places)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        initViews()
+    }
+
+    private fun initViews(){
+        binding.rvPlaces.layoutManager = StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
+        binding.rvPlaces.adapter = placeAdapter
+
+        for (i in Place.PLACE_NAMES.indices) {
+            places.add(Place(Place.PLACE_NAMES[i],        Place.PLACE_RES_DRAWABLE_IDS[i]))
+
+        }
+        placeAdapter.notifyDataSetChanged()
+
     }
 }
