c21afd78bf06a8f4f66b4c6aefffc49ccbc14a8a
KevinHooij <kevin.hooijschuur@hva.nl>
2020-09-29 14:48:36 +0200

    Changes to Step 2 of Level 2 Task 1 since I didn't quite understand what to do at first. Should work now

--- app/src/main/java/com/example/madlevel2task1final/PlaceAdapter.kt
+++ app/src/main/java/com/example/madlevel2task1final/PlaceAdapter.kt
@@ -1,19 +1,37 @@
 package com.example.madlevel2task1final
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.madlevel2task1final.databinding.ItemPlaceBinding
 
-class PlaceAdapter {
+class PlaceAdapter(private val places: List<Place>) : RecyclerView.Adapter<PlaceAdapter.ViewHolder>(){
 
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
         val binding = ItemPlaceBinding.bind(itemView)
 
-        fun databind(place: Place){
-            binding.tvPlaceTitle.text = place.name
+        fun databind(place : Place){
             binding.ivPlaceImage.setImageResource(place.imageResId)
+            binding.tvPlaceTitle.text = place.name
         }
     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_place, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return places.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.databind(places[position])
+    }
+
 
 }
